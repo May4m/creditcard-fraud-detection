06d929fb4b3701cd75d7e9f0ec192d27b12acfc9
Sizwe Mamba <smamba@e5r4p1.wtc.co.za>
2017-08-18 15:26:33 +0200

    added undersampling, oversampling

--- solution.py
+++ solution.py
@@ -13,8 +13,34 @@ from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_
 from sklearn.preprocessing import StandardScaler
 
 
+def oversample_dataset(dataset, oversample_factor=10):
+    """
+    applies oversampling by duplicating the minority dataset
+    """
 
-def load_dataset(filename="creditcard.csv", n_duplicates=10, split_ratio=0.2):
+    dataset = dataset.append(n_duplicates * [dataset[dataset['Class'] == 1]], ignore_index=True)  # oversampling by duplication
+    return 
+
+
+def undersample_dataset(dataset):
+    """
+    Applies undersampling by selecting the number of points in the majority equal to the length of
+    the minority.
+    """
+
+    num_of_fraud_points = len(dataset[dataset['Class'] == 1])
+    fraud_indices = np.array(dataset[dataset['Class'] == 1].index)
+    non_fraud_indices = np.array(dataset[dataset['Class'] == 0].index)
+
+    # randomly select (n=number of fraud sample) samples from the non-fraud subset -> balanced dataset
+    random_non_fraud_indices = np.random.choice(non_fraud_indices, num_of_fraud_points, replace=False)
+    total_undersampled_dataset_indices = np.concatenate([random_non_fraud_indices, fraud_indices])
+    undersampled_dataset = dataset.iloc[total_undersampled_dataset_indices, :]
+    return undersampled_dataset
+
+
+def load_dataset(filename="creditcard.csv", dataset_split_method='train-test',
+                sampling_method='undersampling', train_test_ratio=None, kfolds=None):
     """
     interface function to load the dataset and perform cross-validation splitting
     """
@@ -38,24 +64,29 @@ def load_dataset(filename="creditcard.csv", n_duplicates=10, split_ratio=0.2):
     # preprocessing
     dataset['norm_amount'] = StandardScaler().fit_transform(dataset['Amount'].values.reshape(-1, 1))
     dataset = dataset.drop(['Time','Amount'], axis=1)
-    dataset = dataset.append(n_duplicates * [dataset[dataset['Class'] == 1]], ignore_index=True)  # oversampling by duplication
 
+    # apply sampling technique if selected to tackling class imbalance
+    if sampling_method == 'oversampling':
+        dataset = oversample_dataset(dataset)
+    elif sampling_method == 'undersampling':
+        dataset = undersample_dataset(dataset)
 
-
-    
     features = dataset.ix[:, dataset.columns != 'Class']
     classes = dataset['Class']
 
-    zero = dataset[classes == 0]
+    # train/test split method
+    if dataset_split_method == 'train-test':
+        X_train, X_test, y_train, y_test = train_test_split(features, classes, test_size=train_test_ratio, random_state=0)
+        # remove duplicates in test for accuracy metrics. only when oversampling is applied
+        if sampling_method == 'oversampling':
+            X_test = X_test.drop_duplicates()
+            y_test = y_test[X_test.index]
+        return {'x-train': X_train, 'y-train': y_train, 'x-test': X_test, 'y-test': y_test}, dataset_split_method
 
-    # cross-validation
-    X_train, X_test, y_train, y_test = train_test_split(features, classes, test_size=split_ratio, random_state=0)
-    # remove duplicates in test for accuracy metrics
-    X_test = X_test.drop_duplicates()
-    y_test = y_test[X_test.index]
-
-
-    return {'x-train': X_train, 'y-train': y_train, 'x-test': X_test, 'y-test': y_test, 'zero-class': zero}
+    # kfolds method
+    if dataset_split_method == 'kfolds':
+        kfolds = KFold(len(classes), kfolds, shuffle=False) 
+    return {'features': features, 'classes': classes, 'kfolds': kfolds}, dataset_split_method
 
 
 def accaracy_measures(model, dataset):
@@ -80,17 +111,16 @@ def accaracy_measures(model, dataset):
 
 
 
-def model(dataset):
+def model(datainput, split_method):
     """
     random forest model
     """
 
     # random forest classifiers
-    ran_forest = RandomForestClassifier(n_estimators=13, n_jobs=-1)
-    ran_forest.fit(dataset['x-train'], dataset['y-train'])
+    if split_method == "train-test":
+        dataset = datainput
+        ran_forest = RandomForestClassifier(n_estimators=13, n_jobs=-1)
+        ran_forest.fit(dataset['x-train'], dataset['y-train'])
+        accaracy_measures(ran_forest, dataset)
 
-
-    accaracy_measures(ran_forest, dataset)
-
-
-model(load_dataset(split_ratio=0.2))
+model(*load_dataset())
